da1d2eb4173d3974881dbb9d2a81015a9eaef114
BinuraEN <binuraenugara2@gmail.com>
2023-04-09 22:40:41 +0530

    Added google auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import Button from "@/components/ui/Button";
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  await db.set("hello", "hello");
-  return <div>Home</div>;
+  return <Button>Helo</Button>;
 }
